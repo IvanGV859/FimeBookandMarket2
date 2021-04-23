600714ed3e92b262172ec931df8161f4194941f8
Pedro Candia <52978459+AilingITS@users.noreply.github.com>
2021-04-23 01:13:33 -0500

    Menu y login (Interfaces)

--- app/src/main/java/com/example/fimebookandmarket2/MainActivity.java
+++ app/src/main/java/com/example/fimebookandmarket2/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.fimebookandmarket2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.main_join_now_btn:
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.main_login_btn:
+
+                break;
+        }
     }
 }
