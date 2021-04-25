d33c06966e93228d5e49b7e410cb8aeb4463b8e7
Ivan Gonzalez <se_ivan_859@hotmail.com>
2021-04-25 11:46:04 -0500

    Merge branch 'master' of C:\Users\se_iv\AndroidStudioProjects\COPIAFimeBookandMarket2 with conflicts.

--- app/src/main/java/com/example/fimebookandmarket2/LoginActivity.java
+++ app/src/main/java/com/example/fimebookandmarket2/LoginActivity.java
@@ -2,13 +2,28 @@ package com.example.fimebookandmarket2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText InputNumber, InputPassword;
+    private Button LoginButton;
+    private ProgressDialog loadingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        LoginButton = (Button) findViewById(R.id.login_btn);
+        InputEmail = (EditText) findViewById(R.id.register_email_input);
+        InputPassword = (EditText) findViewById(R.id.login_password_input);
+        InputNumber = (EditText) findViewById(R.id.login_phone_number_input);
+        InputPhone = (EditText) findViewById(R.id.register_phone_input);
+        loadingBar = new ProgressDialog(this);
+
     }
 }
