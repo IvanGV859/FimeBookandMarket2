b464b861d45fd1d73590fec6aba9b816cb72a74f
Pedro Candia <52978459+AilingITS@users.noreply.github.com>
2021-04-24 20:02:12 -0500

    register funcional

--- app/src/main/java/com/example/fimebookandmarket2/RegisterActivity.java
+++ app/src/main/java/com/example/fimebookandmarket2/RegisterActivity.java
@@ -13,6 +13,8 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -26,10 +28,16 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText InputName, InputEmail, InputPassword, InputConfPassword, InputPhone;
     private ProgressDialog loadingBar;
 
+    FirebaseAuth mAuth;
+    DatabaseReference mDatabase;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
         InputName = (EditText) findViewById(R.id.register_username_input);
         InputEmail = (EditText) findViewById(R.id.register_email_input);
@@ -54,77 +62,69 @@ public class RegisterActivity extends AppCompatActivity {
         String confpassword = InputConfPassword.getText().toString();
         String phone = InputPhone.getText().toString();
 
-        if(TextUtils.isEmpty(name)){
-            Toast.makeText(this, "Escribe un nombre...", Toast.LENGTH_SHORT).show();
-        } else if(TextUtils.isEmpty(email)){
-            Toast.makeText(this, "Escribe un correo electronico...", Toast.LENGTH_SHORT).show();
-        } else if(TextUtils.isEmpty(password)){
-            Toast.makeText(this, "Escriba una contraseña...", Toast.LENGTH_SHORT).show();
-        } else if(TextUtils.isEmpty(confpassword)){
-            Toast.makeText(this, "Porfavor confirme su contraseña...", Toast.LENGTH_SHORT).show();
-        } else if(TextUtils.isEmpty(phone)){
-            Toast.makeText(this, "Agrege un numero de telefono.", Toast.LENGTH_SHORT).show();
-        } else if(password.length() < 6) {
-            Toast.makeText(this, "La contraseña debe tener más de 6 caracteres.", Toast.LENGTH_SHORT).show();
-        } else if(name.length() < 5){
-            Toast.makeText(this, "El nombre de usuario debe tener mas de 5 caracteres.", Toast.LENGTH_SHORT).show();
-        } else if(!password.equals(confpassword)){
-            Toast.makeText(this, "Las contraseñas no coinciden.", Toast.LENGTH_SHORT).show();
-        } else {
-            loadingBar.setTitle("Creando cuenta");
-            loadingBar.setMessage("Porfavor espere, mientras validamos sus credenciales.");
-            loadingBar.setCanceledOnTouchOutside(false);
-            loadingBar.show();
+        if(!name.isEmpty() && !email.isEmpty() && !password.isEmpty() && !confpassword.isEmpty() && !phone.isEmpty()) {
+            if (password.length() >= 6) {
+                if(name.length() >=5){
+                    if(password.equals(confpassword)){
+                        loadingBar.setTitle("Creando cuenta");
+                        loadingBar.setMessage("Porfavor espere, mientras validamos sus credenciales.");
+                        loadingBar.setCanceledOnTouchOutside(false);
+                        loadingBar.show();
 
-            ValidatephoneNumber(name, email, password, phone);
+                        ValidatephoneNumber(name, email, password, confpassword, phone);
+                    } else {
+                        Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(this, "El usuario debe tener al menos 5 caracteres", Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Toast.makeText(RegisterActivity.this, "La contraseña debe tener al menos 6 caracteres", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(RegisterActivity.this, "Debe completar todos los campos", Toast.LENGTH_SHORT).show();
         }
     }
 
-    private void ValidatephoneNumber(final String name, final String email, final String password, final String phone) {
-        final DatabaseReference RootRef;
-        RootRef = FirebaseDatabase.getInstance().getReference();
+    private void ValidatephoneNumber(final String name, final String email, final String password, final String confpassword,final String phone) {
 
-        RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if(!(dataSnapshot.child("Users").child(phone).exists())){
-
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
                     HashMap<String, Object> userdataMap = new HashMap<>();
                     userdataMap.put("name", name);
                     userdataMap.put("email", email);
                     userdataMap.put("password", password);
+                    userdataMap.put("confpass", confpassword);
                     userdataMap.put("phone", phone);
 
-                    RootRef.child("users").child(phone).updateChildren(userdataMap)
-                            .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                @Override
-                                public void onComplete(@NonNull Task<Void> task) {
-                                    if(task.isSuccessful()){
-                                        Toast.makeText(RegisterActivity.this, "Cuenta creada correctamente", Toast.LENGTH_SHORT).show();
-                                        loadingBar.dismiss();
-                                        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
-                                        startActivity(intent);
-                                    } else {
-                                        loadingBar.dismiss();
-                                        Toast.makeText(RegisterActivity.this, "Error de conexión: Porfavor intente en un rato...", Toast.LENGTH_SHORT).show();
-                                    }
-                                }
-                            });
+                    String id = mAuth.getCurrentUser().getUid();
 
-                } else {
-                    Toast.makeText(RegisterActivity.this, "El telefono " + phone + " ya existe.", Toast.LENGTH_SHORT).show();
+                    mDatabase.child("Users").child(id).setValue(userdataMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task2) {
+                            //Verificar si registra los datos correctamente en la base de datos
+                            if(task2.isSuccessful()){
+                                Toast.makeText(RegisterActivity.this, "Cuenta creada correctamente", Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+                                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                startActivity(intent);
+                            }
+                            else {
+                                loadingBar.dismiss();
+                                Toast.makeText(RegisterActivity.this, "No se registraron los datos correctamente", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+                else {
                     loadingBar.dismiss();
-                    Toast.makeText(RegisterActivity.this, "Intente con otro numero de telefono.", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(RegisterActivity.this, "No se pudo registrar el usuario.", Toast.LENGTH_SHORT).show();
                     Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
                     startActivity(intent);
                 }
             }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
         });
     }
-
 }
