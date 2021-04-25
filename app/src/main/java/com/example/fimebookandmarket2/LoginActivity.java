1e7cf67f669cedab33dfcd60351feef70906d050
Pedro Candia <52978459+AilingITS@users.noreply.github.com>
2021-04-25 01:33:31 -0500

    login funcional

--- app/src/main/java/com/example/fimebookandmarket2/LoginActivity.java
+++ app/src/main/java/com/example/fimebookandmarket2/LoginActivity.java
@@ -1,14 +1,93 @@
 package com.example.fimebookandmarket2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.fimebookandmarket2.Model.Users;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText InputEmail, InputPassword;
+    private Button LoginButton;
+    private ProgressDialog loadingBar;
+
+    FirebaseAuth mAuth;
+    private String parentDbName = "Users";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        LoginButton = (Button) findViewById(R.id.login_btn);
+        InputPassword = (EditText) findViewById(R.id.login_password_input);
+        InputEmail = (EditText) findViewById(R.id.login_email_input);
+        loadingBar = new ProgressDialog(this);
+
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LoginUser();
+            }
+        });
+
+    }
+
+    private void LoginUser() {
+        //Obtener los datos que escribio el usuario
+        String email = InputEmail.getText().toString();
+        String password = InputPassword.getText().toString();
+
+        //Condiciones para ver si no dejo campos vacios
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this,"Escribe tu correo...", Toast.LENGTH_SHORT).show();
+        } else if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this,"Escribe tu contraseña...", Toast.LENGTH_SHORT).show();
+        } else {
+                loadingBar.setTitle("Cargando cuenta");
+                loadingBar.setMessage("Porfavor espere, mientras validamos sus credenciales.");
+                loadingBar.setCanceledOnTouchOutside(false);
+                loadingBar.show();
+
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        Toast.makeText(LoginActivity.this, "Entrando...", Toast.LENGTH_SHORT).show();
+                        loadingBar.dismiss();
+                        Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(intent);
+                        finish();
+                    }else {
+                        Toast.makeText(LoginActivity.this, "Verificar bien los datos", Toast.LENGTH_SHORT).show();
+                        loadingBar.dismiss();
+                    }
+                }
+            });
+        }
     }
 }
+
