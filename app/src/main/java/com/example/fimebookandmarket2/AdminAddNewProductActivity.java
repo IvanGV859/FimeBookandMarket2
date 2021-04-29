ccfdfefea24c12314cda06435114a32d0b0b36b3
Pedro Candia <52978459+AilingITS@users.noreply.github.com>
2021-04-28 23:27:48 -0500

    video 14 terminado

--- app/src/main/java/com/example/fimebookandmarket2/AdminAddNewProductActivity.java
+++ app/src/main/java/com/example/fimebookandmarket2/AdminAddNewProductActivity.java
@@ -169,7 +169,7 @@ public class AdminAddNewProductActivity extends AppCompatActivity {
                 Task<Uri> uriTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
                     @Override
                     public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
-                        if (task.isSuccessful())
+                        if (!task.isSuccessful())
                         {
                             throw task.getException();
 
