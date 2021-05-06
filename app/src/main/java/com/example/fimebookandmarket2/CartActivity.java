bc47956f58a2fbeedfe3bf2d0511761600809995
yaretzi-2414 <82064326+yaretzi-2414@users.noreply.github.com>
2021-05-06 13:18:31 -0500

    Video 24 minuto 7:29

--- app/src/main/java/com/example/fimebookandmarket2/CartActivity.java
+++ app/src/main/java/com/example/fimebookandmarket2/CartActivity.java
@@ -8,6 +8,13 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.fimebookandmarket2.Model.Cart;
+import com.example.fimebookandmarket2.Prevalent.Prevalent;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class CartActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
@@ -27,5 +34,18 @@ public class CartActivity extends AppCompatActivity {
 
         NextProcessBtn = findViewById(R.id.next_btn);
         txtTotalAmount = findViewById(R.id.total_price);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        final DatabaseReference cartListRef = FirebaseDatabase.getInstance().getReference().child("Cart List");
+
+        FirebaseRecyclerOptions<Cart> options =
+                new FirebaseRecyclerOptions.Builder<Cart>().setQuery(cartListRef.child("User View").child(Prevalent.currentOnlineUser.getId()).child("Productos"), Cart.class).build();
+
+
+        FirebaseRecyclerAdapter<Cart, >
     }
 }
